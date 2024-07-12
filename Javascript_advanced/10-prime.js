f068daa09efd93c3cd653a51372021db4ae203ce
Claire2311 <claire.girard@ieseg.fr>
2024-07-12 18:38:18 +0200

    Task 10

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,19 @@
+function countPrimeNumbers() {
+  let final = [];
+  for (let i = 2; i <= 100; i++) {
+    if (i % 2 === 0) {
+      final.push(i);
+    }
+  }
+  return final.length;
+}
+const t0 = performance.now();
+for (let i = 1; i <= 100; i++) {
+  countPrimeNumbers();
+}
+const t1 = performance.now();
+console.log(
+  "Execution time of calculating prime numbers 100 times was " +
+    (t1 - t0) +
+    " milliseconds."
+);
